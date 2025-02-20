10af9cea61642807ae7687a727b9fdeb117b1ef3
Yauheni <evgeniydodg@gmail.com>
2025-02-20 20:44:19 +0300

    Fix wrong field

--- src/main/java/inc/fabudi/test/dto/Catalog1EntryDTO.java
+++ src/main/java/inc/fabudi/test/dto/Catalog1EntryDTO.java
@@ -1,6 +1,5 @@
 package inc.fabudi.test.dto;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Data;
 
 import java.util.UUID;
@@ -9,7 +8,6 @@ import java.util.UUID;
 public class Catalog1EntryDTO {
     private UUID id;
     private UUID catalogId;
-    private JsonNode data;
     private Integer amount;
     private String description;
 }
